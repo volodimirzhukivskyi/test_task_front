b2983dfbca3be195bcc12308ae4601dc91e58f3f
Zhukivskyi Volodymyr <volodimirzukivskij@gmail.com>
2024-02-29 02:20:54 +0200

    add radia bts, select

---
+++ src/js/modules/create-multiselec.js
@@ -0,0 +1,15 @@
+const multiselect = document.querySelector('.multiselect');
+const button = multiselect.querySelector('.multiselect-button');
+const options = multiselect.querySelector('.multiselect-options');
+
+button.addEventListener('click', (e) => {
+    e.preventDefault()
+  multiselect.classList.toggle('active');
+});
+
+// options.addEventListener('click', (event) => {
+//   const checkbox = event.target.closest('input[type="checkbox"]');
+//   if (checkbox) {
+//     checkbox.checked = !checkbox.checked;
+//   }
+// });
---
+++ src/js/modules/settingsSlider.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.mySwiper', {
+  width: 320,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
